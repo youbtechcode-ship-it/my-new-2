245dc8b9a7dd0491261241ea0ec2bd4af9b2ad56
Bk C <bkc58746@gmail.com>
2025-11-14 14:44:47 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
